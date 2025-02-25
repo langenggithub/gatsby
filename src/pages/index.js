2af386178cd2986928a6e2747a550b106789a24b
langenggithub <125963074+langenggithub@users.noreply.github.com>
2025-02-25 14:21:12 +0800

    Your commit message

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
-import React from "react";
+
 
 const links = [
   {
